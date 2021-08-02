c8dac392e7eeffa8a9d31ca7a4467b91a610ece6
watervin <84309114+watervin@users.noreply.github.com>
2021-08-02 17:25:35 +0900

    0802

---
+++ 10950번.py
@@ -0,0 +1,6 @@
+#10950번
+n=int(input())
+result = 0
+for i in range(1,n+1):
+    result = result + i 
+print(result)
---
+++ 2739번.py
@@ -0,0 +1,4 @@
+#2739번
+n = int(input())
+for i in range(0,10):
+    print(n,"*",i,"=",n*i)
