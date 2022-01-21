e56b331030494497e22c9b089fe16233f49a6931
watervin <84309114+watervin@users.noreply.github.com>
2022-01-21 17:53:24 +0900

    1011번

---
+++ 1월/1011번.py
@@ -0,0 +1,17 @@
+#1011번
+
+t = int(input())
+
+for i in range(t): 
+    x, y = map(int, input().split())
+    distance = y - x
+    n = 0 
+
+    while True: 
+        if distance <= n*(n+1):
+             break
+        n += 1 
+    if distance <= n**2:
+            print(n*2-1) 
+    else: 
+            print(n*2)
