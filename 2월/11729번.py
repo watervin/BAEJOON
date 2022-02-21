f3a666a93a11be42287299a7c371e2c0e48951f1
Subin <subin04171@gmail.com>
2022-02-21 18:12:09 +0900

    11729번

---
+++ 2월/11729번.py
@@ -0,0 +1,14 @@
+#2798번
+
+def hanoi_tower(n, start, end) :
+    if n == 1 :
+        print(start, end)
+        return # 함수 종료
+       
+    hanoi_tower(n-1, start, 6-start-end) # 1단계
+    print(start, end) # 2단계
+    hanoi_tower(n-1, 6-start-end, end) # 목표와 시작을 바꿔
+    
+n = int(input())
+print(2**n-1) #몇번시행?
+hanoi_tower(n, 1, 3)
